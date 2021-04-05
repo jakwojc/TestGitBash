71298ef591224a8f1ec535e09d0eaa48612181ee
Jakub Wojciechowski <74016079+jakwojc@users.noreply.github.com>
2021-04-05 23:02:01 +0200

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e "To jest przykladowy program ktory nic nie robi ale zostal wlasnie zmodyfikowany\n"
+echo -e "To jest przykladowy program który nic nie robi ale zostal wlasnie zmodyfikowany\n"
 echo -e "12+12=$((12+12))"
